33a3b98b7cbb4a82b6a7e3ecf2a857e8730a1e46
Matias Ezequiel Roel <roel@fktech.net>
2025-02-11 18:44:30 -0300

    Se elimina CORS en route -  Problema de CORS

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -1,26 +1,13 @@
 import nodemailer from "nodemailer";
 
 export async function POST(req) {
-  // Configurar cabeceras CORS
-  const headers = new Headers({
-    "Access-Control-Allow-Origin": "https://cartosys.com.ar/",
-    "Access-Control-Allow-Methods": "POST, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
-  });
-
-  // Manejar solicitudes preflight (OPTIONS)
-  if (req.method === "OPTIONS") {
-    return new Response(null, { status: 204, headers });
-  }
-
-  // Procesar solicitud POST
   try {
     const { name, email, message } = await req.json();
 
     if (!name || !email || !message) {
       return new Response(
         JSON.stringify({ message: "Todos los campos son obligatorios." }),
-        { status: 400, headers }
+        { status: 400 }
       );
     }
 
@@ -44,13 +31,13 @@ export async function POST(req) {
 
     return new Response(
       JSON.stringify({ message: "Correo enviado con éxito." }),
-      { status: 200, headers }
+      { status: 200 }
     );
   } catch (error) {
     console.error("Error al enviar el correo:", error);
     return new Response(
       JSON.stringify({ message: "Error al enviar el correo." }),
-      { status: 500, headers }
+      { status: 500 }
     );
   }
 }
